20f87c5295ac2f0880ab7f8fec5541832bf84e29
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2025-11-24 04:01:53 +0300

    Initial commit

---
+++ lesson7.py
@@ -0,0 +1,46 @@
+import os
+import ptbot
+import random
+from dotenv import load_dotenv
+from pytimeparse import parse
+
+
+bot = None
+
+def wait(chat_id, message):
+    global bot
+    message_id = bot.send_message(chat_id, "Запускаю таймер...")
+    timer = parse(message)
+    bot.create_countdown(timer, notify_progress, chat_id=chat_id, message_id=message_id, total=timer)
+    bot.create_timer(timer, choose, chat_id=chat_id)
+
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+
+def notify_progress(secs_left, chat_id, message_id, total):
+    global bot
+    progress_bar = render_progressbar(total=total, iteration=total - secs_left)
+    text_message = f'Осталось {secs_left} секунд\n{progress_bar}'
+    bot.update_message(chat_id, message_id, text_message)
+
+def choose(chat_id):
+    global bot
+    bot.send_message(chat_id, 'Время вышло!')
+
+
+def main():
+    global bot
+    load_dotenv()
+    telegram_token = os.getenv('TG_TOKEN')
+    telegram_chat_id = os.getenv('TG_CHAT_ID')
+    bot = ptbot.Bot(telegram_token)
+    bot.reply_on_message(wait)
+    bot.run_bot()
+
+if __name__ == '__main__':
+    main()
