74f3353387597cd4bbdef7f2b175955ab8e2636b
IgorVinogradov1 <igor.vinogradov1@yandex.com>
2025-12-01 01:46:37 +0300

    Update lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -3,16 +3,14 @@ import ptbot
 import random
 from dotenv import load_dotenv
 from pytimeparse import parse
+from functools import partial
 
 
-bot = None
-
-def wait(chat_id, message):
-    global bot
+def wait(bot, chat_id, message):
     message_id = bot.send_message(chat_id, "Запускаю таймер...")
     timer = parse(message)
-    bot.create_countdown(timer, notify_progress, chat_id=chat_id, message_id=message_id, total=timer)
-    bot.create_timer(timer, choose, chat_id=chat_id)
+    bot.create_countdown(timer, partial(notify_progress, bot), chat_id=chat_id, message_id=message_id, total=timer)
+    bot.create_timer(timer, partial(choose, bot), chat_id=chat_id)
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -22,24 +20,24 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
-def notify_progress(secs_left, chat_id, message_id, total):
-    global bot
+def notify_progress(bot, secs_left, chat_id, message_id, total):
     progress_bar = render_progressbar(total=total, iteration=total - secs_left)
     text_message = f'Осталось {secs_left} секунд\n{progress_bar}'
     bot.update_message(chat_id, message_id, text_message)
 
-def choose(chat_id):
-    global bot
+def choose(bot, chat_id):
     bot.send_message(chat_id, 'Время вышло!')
 
-
 def main():
-    global bot
     load_dotenv()
     telegram_token = os.getenv('TG_TOKEN')
     telegram_chat_id = os.getenv('TG_CHAT_ID')
     bot = ptbot.Bot(telegram_token)
-    bot.reply_on_message(wait)
+
+    def handle_message(chat_id, message):
+        wait(bot, chat_id, message)
+
+    bot.reply_on_message(handle_message)
     bot.run_bot()
 
 if __name__ == '__main__':
